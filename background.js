6329ded2e864fcf17fca369df36958cec1be05b8
sam8beard <sammybeard2002@gmail.com>
2024-11-04 12:24:02 -0500

    installing react

--- background.js
+++ background.js
@@ -1,20 +1,20 @@
-chrome.runtime.onInstalled.addListener(() => { 
-    chrome.action.setBadgeText({ 
-        "text": "OFF",
-    });
-})
+// chrome.runtime.onInstalled.addListener(() => { 
+//     chrome.action.setBadgeText({ 
+//         "text": "OFF",
+//     });
+// })
 
-const chat = "https://chatgpt.com/"; 
-const webstore = "https://developer.chrome.com/docs/webstore";
+// const chat = "https://chatgpt.com/"; 
+// const webstore = "https://developer.chrome.com/docs/webstore";
 
-chrome.action.onClicked.addListener(async (tab) => { 
-    if (tab.url.startsWith(chat) || tab.url.startsWith(webstore)) { 
-        const prevState = await chrome.action.getBadgeText({tabId: tab.id});
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+// chrome.action.onClicked.addListener(async (tab) => { 
+//     if (tab.url.startsWith(chat) || tab.url.startsWith(webstore)) { 
+//         const prevState = await chrome.action.getBadgeText({tabId: tab.id});
+//         const nextState = prevState === 'ON' ? 'OFF' : 'ON'
 
-        await chrome.action.setBadgeText({ 
-            tabId: tab.id,
-            text: nextState,
-        });
-    } 
-})
+//         await chrome.action.setBadgeText({ 
+//             tabId: tab.id,
+//             text: nextState,
+//         });
+//     } 
+// })
--- content.js
+++ content.js
@@ -1,8 +1,37 @@
-$('body').append(`<h1> Hello World </h1>`)
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// Retrieve url for image 
+// $('body').append(`<h1> Hello World </h1>`)
+
+//  
+// iconURL = chrome.runtime.getURL("./bottle.png");
+
+// // Append image to DOM
+// $('body').prepend(`<img class="bottle-img" src="${iconURL}" alt="bottle image">`)
+
+// $(`<img class="bottle-img" src="${iconURL}" alt="bottle image">`).insertAfter(`body > div.relative.flex.h-full.w-full.overflow-hidden.transition-colors.z-0 > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden`)
+
+// Retrieve url for image
 iconURL = chrome.runtime.getURL("./bottle.png");
 
-// Append image to DOM
-$('body').prepend(`<img class="bottle-img" src="${iconURL}" alt="bottle image">`)
-$('.bottle-img').css('mix-blend-mode', 'multiply')
+// Render image 
+function waterBottle() { 
+    return ( 
+        <div> 
+            <img class="bottle-img" src="${iconURL}" alt="bottle image"/>
+        </div>
+        
+    )
+    
+}
+function injectBottle() { 
+    const target = document.getElementByClassName(`body > div.relative.flex.h-full.w-full.overflow-hidden.transition-colors.z-0 > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden`)
+
+    const divToAppend = document.createElement(`div`)
+    
+    target.appendChild(divToAppend)
+
+    ReactDOM.render(<waterBottle/>, divToAppend) 
+}
+
+injectBottle() 
