6c76052107ac9bf98f7720c88a8f8ba551b5c7a2
sam8beard <sammybeard2002@gmail.com>
2024-12-17 16:12:03 -0500

    restructured directory

---
+++ dist/contentScript.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";chrome.runtime.onMessage.addListener((function(e,o,t){var n=document.createElement("h1"),c=document.createTextNode("Please contact: ".concat(e));n.appendChild(c),document.body.appendChild(n),t("Looks good to me bro!")}))})();
